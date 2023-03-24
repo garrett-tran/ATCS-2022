4790ee01004d9b204182814a67ea5d29df8f7f6c
garrett-tran <your email here>
2023-03-24 09:53:01 -0700

    Day one changes to models

--- twitter/models.py
+++ twitter/models.py
@@ -25,6 +25,11 @@ class User(Base):
                              primaryjoin="User.username==Follower.following_id",
                              secondaryjoin="User.username==Follower.follower_id",
                              overlaps="following")
+    tweets = relationship("Tweet", back_populates="user")
+    
+    def __init__(self, username, password):
+        self.username = username
+        self.password = password
 
 
 class Follower(Base):
@@ -35,14 +40,46 @@ class Follower(Base):
     follower_id = Column('follower_id', TEXT, ForeignKey('users.username'))
     following_id = Column('following_id', TEXT, ForeignKey('users.username'))
 
+    def __init__(self, id, follower_id, following_id):
+        self.id = id
+        self.follower_id = follower_id
+        self.following_id = following_id
+
 class Tweet(Base):
-    # TODO: Complete the class
-    pass
+    __tablename__ = "tweets"
+
+
+    id = Column("id", INTEGER, primary_key=True)
+    content = Column('content', TEXT, nullable=False)
+    timestamp = Column('timestamp', TEXT, nullable=False)
+    username = Column('username',TEXT, ForeignKey('users.username'))
+
+    def __init__(self, id, content, timestamp, username):
+        self.id = id
+        self.content = content
+        self.timestamp = timestamp
+        self.username = username
+
+    user = relationship("User", back_populates="tweet")
 
 class Tag(Base):
-    # TODO: Complete the class
-    pass
+    __tablename__ = "tags"
+
+    id = Column("id", INTEGER, primary_key=True)
+    content = Column('content', TEXT, nullable=False)
+    
+    def __init__(self, id, content):
+        self.id = id
+        self.content = content
 
 class TweetTag(Base):
-    # TODO: Complete the class
-    pass
+    __tablename__ = "tweettags"
+
+    id = Column("id", INTEGER, primary_key=True)
+    tweet_id = ("tweet_id", INTEGER, ForeignKey('tweets.id'))
+    tag_id = ("tag_id", INTEGER, ForeignKey('tags.id'))
+
+    def __init__(self, id, tweet_id, tag_id):
+        self.id = id
+        self.tweet_id = tweet_id
+        self.tag_id = tag_id
